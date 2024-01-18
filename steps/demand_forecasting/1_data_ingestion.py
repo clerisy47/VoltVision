7cc05afeed9d7b109d7ba8d28011752dc254d425
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-01-18 09:35:03 +0545

    add ingest pipelines for both datasets

---
+++ steps/demand_forecasting/1_data_ingestion.py
@@ -0,0 +1,37 @@
+import logging
+
+import pandas as pd
+from zenml import step
+
+
+class IngestData:
+    """
+    Data ingestion class which ingests data from the source and returns a DataFrame.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the data ingestion class."""
+        pass
+
+    def get_data(self) -> pd.DataFrame:
+        df_demand = pd.read_csv('../../dataset/Demand Forecasting/Demand Forecasting Demand Data upto Feb 21.csv', sep=',')
+        df_weather = pd.read_csv('../../dataset/Demand Forecasting/Demand Forecasting Weather Data upto Feb 28.csv', sep=',')
+        df_merged=pd.merge(left=df_demand,right=df_weather, on='datetime')
+        return df_merged
+
+
+@step
+def ingest_data() -> pd.DataFrame:
+    """
+    Args:
+        None
+    Returns:
+        df: pd.DataFrame
+    """
+    try:
+        ingest_data = IngestData()
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(e)
+        raise e
